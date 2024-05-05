2586fb89a17003099ac0796ca5494e8588aa27aa
PiotrSolecki <127014084+PiotrSolecki@users.noreply.github.com>
2024-05-05 11:33:18 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,10 @@
+terraform {
+  backend "local" {
+      path = "/.terraform.tfstate"
+  }
+}
 
+resource "local_file" "hello" {
+  content  = "print('${var.message}')"
+  filename = "${var.file_path}"
+}
